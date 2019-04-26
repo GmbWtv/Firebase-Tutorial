febb41140a73a3b42461db4d331710cda15fb8fa
GmbWtv <45470911+GmbWtv@users.noreply.github.com>
2019-04-26 16:30:30 +0100

    Update Database
    It now updates de database

--- Firebase_Youtube/Form1.Designer.cs
+++ Firebase_Youtube/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -124,11 +125,22 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(476, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 54);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(570, 412);
+            this.ClientSize = new System.Drawing.Size(651, 412);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
@@ -159,6 +171,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Firebase_Youtube/Form1.cs
+++ Firebase_Youtube/Form1.cs
@@ -70,5 +70,20 @@ namespace Firebase_Youtube
             //something
             //something else
         }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            var data = new Data
+            {
+                Id=textBox1.Text,
+                Name=textBox2.Text,
+                Address=textBox3.Text,
+                Age=textBox4.Text
+            };
+
+            FirebaseResponse response = await client.UpdateTaskAsync("Information/"+textBox1.Text,data);
+            Data result = response.ResultAs<Data>();
+            MessageBox.Show("Data Updated at ID: "+ result.Id);
+        }
     }
 }
